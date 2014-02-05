eec33e13fd3b4ecff0644184d3135b035bcb2c88
Dana Scheider <dana.scheider@gmail.com>
2014-02-05 13:26:54 -0800

    Changed step definition for 'I clock in' to use Log class

--- features/step_definitions/timesheet_saves_timesheet_steps.rb
+++ features/step_definitions/timesheet_saves_timesheet_steps.rb
@@ -1,10 +1,13 @@
+require 'log'
+require 'etc'
+
 Given "there is no directory called $name" do |name|
   File.directory?(name) == nil
 end
 
 When /^I clock in$/ do 
-  @shift = Shift.new
-  @shift.in
+  @timesheet = Log.new
+  @timesheet.in
 end
 
 Then /^a directory called "\S*\/\.?\w+\/?" should be created$/ do |name|
@@ -13,6 +16,6 @@ end
 
 And /^the time should be logged in a file called "\S*\/\.?\w+\/\S+"$/ do |name|
   File.open(name, "r+") do |file|
-    file.puts(@shift.time_in)
+    file.puts(@timesheet.time_in)
   end
 end
