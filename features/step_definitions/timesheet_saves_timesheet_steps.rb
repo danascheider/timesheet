42a580b2dee7a1c2baa6dd1ee5a17d9a31bb6c1e
Dana Scheider <dana.scheider@gmail.com>
2014-02-04 16:33:53 -0800

    Added final step definition for Scenario 1 in timesheet_saves_timesheet

--- features/step_definitions/timesheet_saves_timesheet_steps.rb
+++ features/step_definitions/timesheet_saves_timesheet_steps.rb
@@ -10,3 +10,9 @@ end
 Then /^A directory called "^\S*\/\.?\w+\/?" should be created$/ do |name|
   Dir.mkdir(name)
 end
+
+And /^the time should be logged in a file called "^\S*\/\.?\w+\/\S+"$/ do |name|
+  File.open(name, "r+") do |file|
+    file.puts(@shift.time_in)
+  end
+end
