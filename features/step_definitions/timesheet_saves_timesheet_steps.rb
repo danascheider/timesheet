ac4a3cb12b23327303446750b502e3dd868ac7b2
Dana Scheider <dana.scheider@gmail.com>
2014-02-07 20:54:19 -0800

    Modified step definition to check the last line of the file ~/.timesheet/timesheet

--- features/step_definitions/timesheet_saves_timesheet_steps.rb
+++ features/step_definitions/timesheet_saves_timesheet_steps.rb
@@ -6,15 +6,17 @@ Given /^there is no directory called (.*)$/ do |name|
 end
 
 When /^I clock in$/ do 
-  @action = `bundle exec bin/timesheet in`
+  @action = `bundle exec bin/timesheet -u dscheider in -t '2014-02-07 17:27:00 -0800'`
 end
 
 Then /^a directory called (.*) should be created$/ do |directory|
   File.exist?(directory)
 end
 
-And /^the time should be logged in a file called "\S*\/\.?\w+\/\S+"$/ do |name|
-  File.open(name, "r+") do |file|
-    file.puts(@timesheet.time_in)
-  end
+And /^the time should be logged in a file called (.*)$/ do |file|
+  raise("Error: No such file") unless File.exists?(file)
+  @arr = IO.readlines(file)
+  @arr[-1].should == "dscheider in  => 2014-02-07 17:27:00 -0800"
+  # The file ~/.timesheet/timesheet should exist and the last line should be 
+  # dscheider in  => 2014-02-07 17:27:00 -0800
 end
