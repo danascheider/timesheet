b680c00b820192349c597de4628822dc70a26ffc
Dana Scheider <dana.scheider@gmail.com>
2014-02-09 15:27:55 -0800

    Added second argument to 'there is no directory called (.*)' so it can accommodate the opposite test - may not be consistent with best practices

--- features/step_definitions/timesheet_saves_timesheet_steps.rb
+++ features/step_definitions/timesheet_saves_timesheet_steps.rb
@@ -1,8 +1,9 @@
 require 'log'
 require 'etc'
 
-Given /^there is no directory called (.*)$/ do |name|
-  File.directory?(name) == nil
+Given /^there is no directory called (.*)$/ do |article,dirname|
+  if article == "a" then File.directory?(dirname);
+  else !File.directory?(dirname)
 end
 
 When /^I clock in$/ do 
@@ -13,8 +14,16 @@ Then /^a directory called (.*) should be created$/ do |directory|
   File.exist?(directory)
 end
 
-And /^the time should be logged in a file called (.*)$/ do |file|
+And /^a file called (.*) should be created$/ do |file|
+  #
+end
+
+And /^the time should be appended to the file (.*)$/ do |file|
   raise("Error: No such file") unless File.exists?(file)
   @arr = IO.readlines(file)
   @arr[-1].should == "dscheider in  => 2014-02-07 17:27:00 -0800"
+end
+
+But /^there is no file called (.*)$/ do |name|
+  File.exists?(name) == nil
 end
