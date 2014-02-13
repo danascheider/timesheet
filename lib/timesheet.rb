4f939d70222ce68d6a46f1c9d7dfcfeee6e16e57
Dana Scheider <dana.scheider@gmail.com>
2014-02-12 17:17:34 -0800

    Added require_relative 'user'

--- lib/timesheet.rb
+++ lib/timesheet.rb
@@ -1,5 +1,6 @@
 require 'timesheet/version.rb'
 require_relative 'log'
+require_relative 'user'
 
 # Add requires for other files you add to your project here, so
 # you just need to require this one file in your bin file
