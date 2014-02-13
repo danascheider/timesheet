a98388c821707b220e5e945cc4343ea15c425942
Dana Scheider <dana.scheider@gmail.com>
2014-02-12 17:14:55 -0800

    Added @username instance variable instead of using @user.name when needed

--- features/step_definitions/clocking_in_steps.rb
+++ features/step_definitions/clocking_in_steps.rb
@@ -3,6 +3,7 @@ require 'user'
 
 Given /^I am a Timesheet user$/ do 
   @user = User.new
+  @username = @user.name
 end
 
 Given /^I am currently (in|out)$/ do |status|
@@ -10,12 +11,11 @@ Given /^I am currently (in|out)$/ do |status|
 end
 
 When /^I clock (in|out)$/ do |action|
-  `bundle exec bin/timesheet -u "#{@user.name} #{action}"`
+  `bundle exec bin/timesheet -u "#{@username} #{action}"`
 end
 
 Then /^my username and time (in|out) should be posted on my timesheet$/ do |status|
-  @timesheet = Log.new(@user)
-  @timesheet.write("#{@user.name} #{status} => #{Time.now}\n")
+  @user.mklog(@username)
 end
 
 Then /^I should see a message that I am already clocked (in|out)$/ do |status|
