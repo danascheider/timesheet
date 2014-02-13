8cbaa0d1e9add2382e36137d7f379502bb3c8f87
Dana Scheider <dana.scheider@gmail.com>
2014-02-12 17:18:33 -0800

    Added #mklog and #adopt to attach log files to User object Defined #post, #in, and #out to make log entries Added require_relative 'log'

--- lib/user.rb
+++ lib/user.rb
@@ -1,6 +1,7 @@
 # There is an instance of the User class for each user.
 
 require 'etc'
+require_relative 'log'
 
 class User
   attr_reader :name
@@ -11,15 +12,40 @@ class User
     @status = status
   end
 
-  def in
+  def adopt(file)
+    # raise some error if user doesn't have the right permissions
+    @log_file = file
+  end
+
+  def mklog(username=@name,log_path=nil)
+    @log_path = log_path || "/home/#{username}/.timesheet"
+    File.exist? @log_path || Dir.mkdir(@log_path)
+    if File.exists?(@log_file = "#{@log_path}/timesheet") 
+      adopt(@log_file);
+    else 
+      Log.open(@log_file,"a+") do |file|
+        file.puts "TIMESHEET FOR #{@name.upcase}"
+      end
+    end
+  end
+
+  def in(quiet=nil)
     @status = 'in'
+    post('in') unless quiet
   end
 
   def in?
     @status == 'in'
   end
 
-  def out
+  def out(quiet=nil)
     @status = 'out'
+    post('out') unless quiet
+  end
+
+  def post(in_or_out,time=Time.now)
+    raise ArgumentError("Invalid status #{in_or_out}") unless in_or_out == ('in' || 'out')
+    mklog unless @log_file
+    @log_file.puts("#{@name} #{in_or_out} => #{time}")
   end
 end
