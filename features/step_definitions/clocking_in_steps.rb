e63746e745b3bba9a00dcea00d27ff1722e91d17
Dana Scheider <dana.scheider@gmail.com>
2014-02-14 18:10:33 -0800

    Conflated 'When I clock in' and 'When I clock out' into one step

--- features/step_definitions/clocking_in_steps.rb
+++ features/step_definitions/clocking_in_steps.rb
@@ -10,14 +10,8 @@ When /^I clock (in|out)$/ do |status|
   `bundle exec bin/timesheet #{status}`
 end
 
-Then /^my username and time in should be posted to my timesheet$/ do 
+Then /^my username and time (in|out) should be posted to my timesheet$/ do |status|
   File.open("/home/#{@username}/.timesheet/timesheet",'r+') do |file|
-    IO.readlines(file)[-1].should eql "#{@username} in  => #{Time.now}\n"
-  end
-end
-
-Then /^my username and time out should be posted to my timesheet$/ do 
-  File.open("/home/#{@username}/.timesheet/timesheet",'r+') do |file|
-    IO.readlines(file)[-1].should eql "#{@username} out => #{Time.now}\n"
+    IO.readlines(file)[-1].should eql "#{@username} #{status} => #{Time.now}\n"
   end
 end
