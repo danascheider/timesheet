1f675c437376b5835dae35f665b8b6eb6cb02a40
Dana Scheider <dana.scheider@gmail.com>
2014-02-15 12:17:04 -0800

    Added step definitions for steps in scenario 'Time in differs from present time'

--- features/step_definitions/clocking_in_steps.rb
+++ features/step_definitions/clocking_in_steps.rb
@@ -10,6 +10,16 @@ When /^I clock (in|out)$/ do |status|
   `bundle exec bin/timesheet #{status}`
 end
 
+When /^I clock (in|out) with the time set to (\d{4}(\-\d\d){2} (\d{2}:){2}\d{2} \-\d{4})$/ do |status,date,time,zone|
+  `bundle exec bin/timesheet #{status} -t "#{date} #{time} #{zone}"`
+end
+
+Then /^the time (in|out) on my timesheet should be (\d{4}(\-\d\d){2} (\d{2}:){2}\d{2} \-\d{4})$/ do |status,date,time,zone|
+  File.open("/home/#{@username}/.timesheet/timesheet",'r+') do |file|
+    IO.readlines(file)[-1].should =~ /#{status}.*#{date} #{time} #{zone}\n?$/
+  end
+end
+
 Then /^my username and time (in|out) should be posted to my timesheet$/ do |status|
   # Keep columns the same width for in and out statuses
   if status == "in" then status = "in "; end
