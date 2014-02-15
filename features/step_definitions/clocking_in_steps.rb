81205dcd640fd58dc4af299db941bf42e93c62b9
Dana Scheider <dana.scheider@gmail.com>
2014-02-14 18:17:40 -0800

    Conflated 'Then my username and time in/out should be posted to my timesheet'

--- features/step_definitions/clocking_in_steps.rb
+++ features/step_definitions/clocking_in_steps.rb
@@ -11,6 +11,8 @@ When /^I clock (in|out)$/ do |status|
 end
 
 Then /^my username and time (in|out) should be posted to my timesheet$/ do |status|
+  # Keep columns the same width for in and out statuses
+  if status == "in" then status = "in "; end
   File.open("/home/#{@username}/.timesheet/timesheet",'r+') do |file|
     IO.readlines(file)[-1].should eql "#{@username} #{status} => #{Time.now}\n"
   end
