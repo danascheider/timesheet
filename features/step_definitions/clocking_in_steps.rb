2d33324faea27304ab1b299a1e977b58c7b82b87
Dana Scheider <dana.scheider@gmail.com>
2014-02-14 17:44:00 -0800

    Added step definitions for clocking out steps

--- features/step_definitions/clocking_in_steps.rb
+++ features/step_definitions/clocking_in_steps.rb
@@ -10,8 +10,18 @@ When /^I clock in$/ do
   `bundle exec bin/timesheet in`
 end
 
+When /^I clock out$/ do 
+  `bundle exec bin/timesheet out`
+end
+
 Then /^my username and time in should be posted to my timesheet$/ do 
   File.open("/home/#{@username}/.timesheet/timesheet",'r+') do |file|
     IO.readlines(file)[-1].should eql "#{@username} in  => #{Time.now}\n"
+  end
+end
+
+Then /^my username and time out should be posted to my timesheet$/ do 
+  File.open("/home/#{@username}/.timesheet/timesheet",'r+') do |file|
+    IO.readlines(file)[-1].should eql "#{@username} out => #{Time.now}\n"
   end
 end
