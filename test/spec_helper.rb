b6d58103852275ff6caca37b6d9370ee01269253
Dana Scheider <dana.scheider@gmail.com>
2014-02-16 12:45:10 -0800

    Changed 'test' to 'spec' in filenames

---
+++ test/spec_helper.rb
@@ -0,0 +1,11 @@
+require 'test/unit'
+require 'lib/user.rb'
+require 'lib/log.rb'
+
+# Add test libraries you want to use here, e.g. mocha
+
+class Test::Unit::TestCase
+
+  # Add global extensions to the test case class here
+  
+end
