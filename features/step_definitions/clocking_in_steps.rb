123375a40d212eff3db0883a061d73c2939e2caf
Daniel Dreier <ddreier@thinkplango.com>
2014-02-18 18:05:47 -0800

    Fix user home directory assumption Switch from /home/$user to $HOME env variable

--- features/step_definitions/clocking_in_steps.rb
+++ features/step_definitions/clocking_in_steps.rb
@@ -15,7 +15,7 @@ When /^I clock (in|out) with the time set to (\d{4}(\-\d\d){2} (\d{2}:){2}\d{2} 
 end
 
 Then /^the time (in|out) on my timesheet should be (\d{4}(\-\d\d){2} (\d{2}:){2}\d{2} \-\d{4})$/ do |status,date,time,zone|
-  File.open("/home/#{@username}/.timesheet/timesheet",'r+') do |file|
+  File.open("#{ENV['HOME']}/.timesheet/timesheet",'r+') do |file|
     IO.readlines(file)[-1].should =~ /#{status}.*#{date} #{time} #{zone}\n?$/
   end
 end
@@ -23,7 +23,7 @@ end
 Then /^my username and time (in|out) should be posted to my timesheet$/ do |status|
   # Keep columns the same width for in and out statuses
   if status == "in" then status = "in "; end
-  File.open("/home/#{@username}/.timesheet/timesheet",'r+') do |file|
+  File.open("#{ENV['HOME']}/.timesheet/timesheet",'r+') do |file|
     IO.readlines(file)[-1].should eql "#{@username} #{status} => #{Time.now}\n"
   end
 end
